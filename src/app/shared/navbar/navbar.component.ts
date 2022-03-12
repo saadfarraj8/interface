207ecc4a22d629897d0173da7d93c8d26fbd5eab
Saad <saadfarraj8@gmail.com>
2022-03-12 12:42:58 +0100

    Insert test

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -34,16 +34,16 @@ export class NavbarComponent implements OnInit{
        });
     }
     getTitle(){
-      // var titlee = this.location.prepareExternalUrl(this.location.path());
-      // if(titlee.charAt(0) === '#'){
-      //     titlee = titlee.slice( 1 );
-      // }
-      // for(var item = 0; item < this.listTitles.length; item++){
-      //     if(this.listTitles[item].path === titlee){
-      //         return this.listTitles[item].title;
-      //     }
-      // }
-      return 'DOCUMANIA';
+      var titlee = this.location.prepareExternalUrl(this.location.path());
+      if(titlee.charAt(0) === '#'){
+          titlee = titlee.slice( 1 );
+      }
+      for(var item = 0; item < this.listTitles.length; item++){
+          if(this.listTitles[item].path === titlee){
+              return this.listTitles[item].title;
+          }
+      }
+      
     }
     sidebarToggle() {
         if (this.sidebarVisible === false) {
